2c64e9a717701fb4ac49a347e32defd7279d8efd
Tiffany Chen <tiffanysta.c@gmail.com>
2021-05-14 17:15:54 -0400

    quick print

--- data_processing.py
+++ data_processing.py
@@ -33,6 +33,8 @@ def clean_data(data):
     cleaned_data = cleaned_data[cleaned_data["Lyrics"] != ""]
 
     # Remove datapoints with bad lyrics
+    print(cleaned_data['Lyrics'])
+
     cleaned_data = cleaned_data.loc[~cleaned_data["Lyrics"].str.contains("---")]
     cleaned_data = cleaned_data.loc[~cleaned_data.str.contains("___")] 
     cleaned_data = cleaned_data.loc[~cleaned_data.str.contains("|"))]
